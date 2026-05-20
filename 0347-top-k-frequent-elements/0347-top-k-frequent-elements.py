fc5bbd09615ff980a2d44c5e1c1702f54ee66a1f
IBH <124177073+NukGod-V@users.noreply.github.com>
2026-05-21 01:00:15 +0530

    Time: 11 ms (24.13%), Space: 24.9 MB (12.26%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,33 +1,16 @@
-# class Solution:
-#     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # res = {}
-        # resl = []
-        # for i in nums:
-        #     res[i] = res.get(i,0) + 1
-        
-        # sort_dic = dict(sorted(res.items(),key = lambda, item: item[1], reverse=True))
-        # i = 0
-        # for e,n in sort_dic:
-            
-
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = {}
-        # List of lists to store numbers by their frequency
-        freq = [[] for _ in range(len(nums) + 1)]
-        
-        # Count frequencies: O(n)
-        for n in nums:
-            count[n] = count.get(n, 0) + 1
-            
-        # Place numbers into buckets: O(n)
-        for n, c in count.items():
-            freq[c].append(n)
-            
-        res = []
-        # Collect top k: O(n)
-        for i in range(len(freq) - 1, 0, -1):
-            for n in freq[i]:
-                res.append(n)
-                if len(res) == k:
-                    return res
+        res = {}
+        resl = [[] for i in range(len(nums)+1)]
+
+        for i in nums:
+            res[i] = 1 + res.get(i,0)
+        for i,n in res.items():
+            resl[n].append(i)
+        main_res = []
+
+        for i in range(len(resl) -1,0,-1):
+            for v in resl[i]:
+                main_res.append(v)
+                if len(main_res) == k:
+                    return main_res
